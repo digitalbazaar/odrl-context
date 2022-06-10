2793b7d3057ac1105991d65387de1b2fc8cd3f1f
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-06-10 10:11:43 -0400

    Fix linting issues and add test.

---
+++ test/context.spec.js
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+const chai = require('chai');
+chai.should();
+const {expect} = chai;
+
+const {
+  contexts, constants, CONTEXT_URL_V1, CONTEXT_V1
+} = require('..');
+
+const contextUrl = constants.CONTEXT_URL_V1;
+
+describe('VC Status List Context', () => {
+  it('constants', async () => {
+    expect(constants).to.exist;
+    expect(CONTEXT_URL_V1).to.exist;
+    expect(CONTEXT_V1).to.exist;
+  });
+
+  it('contexts', async () => {
+    expect(contexts.get(contextUrl)).to.have.property('@context');
+  });
+});
